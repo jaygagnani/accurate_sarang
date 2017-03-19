c6a646069024aaf2cfa81644a06e310621995a17
jay gagnani <jay.gagnani94@gmail.com>
2017-03-19 19:05:53 +0530

    first commit

---
+++ assets/js/scripts1.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  window.sr = ScrollReveal({reset: true});
+  sr.reveal('.banner-top');
+  sr.reveal('.section-1');
+  sr.reveal('.card-1');
+  sr.reveal('.card-2');
+  sr.reveal('.section-2');
+  sr.reveal('.section-3');
+  sr.reveal('.section-4');
+  sr.reveal('.section-5');
+});
